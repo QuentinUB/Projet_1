28ef22ec88aeb1d922b0f23c61feb80ae38c6442
QuentinUB <quentin.bernard58@gmail.com>
2025-10-16 14:13:17 +0200

    Update projet_1_intermediaire.c

--- projet_1_intermediaire.c
+++ projet_1_intermediaire.c
@@ -5,37 +5,25 @@ int main ()
 	float D;
 	printf("quels est l'operation souhaitC)e?\n 1 -> addition \n 2 -> soustraction \n 3 -> multiplication \n 4 -> division\n");
 	scanf("%d",&C);
+	printf("quels est le nbr A ?\n");
+	scanf("%d",&A);
+	printf("quels est le nbr B ?\n");
+	scanf("%d",&B);
 	switch (C)
 	{
 	    case 1:
-		printf("quels est le nbr A ?\n");
-		scanf("%d",&A);
-		printf("quels est le nbr B ?\n");
-		scanf("%d",&B);
 		D=A+B;
 		printf("la somme des deux nombres est:\n%.2f",D);
 		break;
 	    case 2:
-		printf("quels est le nbr A ?\n");
-		scanf("%d",&A);
-		printf("quels est le nbr B ?\n");
-		scanf("%d",&B);
 		D=A-B;
 		printf("la difference des deux nombres est:\n%.2f",D);
 		break;
 	    case 3:
-		printf("quels est le nbr A ?\n");
-		scanf("%d",&A);
-		printf("quels est le nbr B ?\n");
-		scanf("%d",&B);
 		D=A*B;
 		printf("le produit des deux nombres est:\n%.2f",D);
 	    break;
 	    case 4:
-		printf("quels est le nbr A ?\n");
-		scanf("%d",&A);
-		printf("quels est le nbr B ?\n");
-		scanf("%d",&B);
 		D=(float)A/B;
 		printf("la division des deux nombres est:\n%.2f",D);
 	    break;
@@ -45,3 +33,4 @@ int main ()
     }
 return 0;
 }
+
